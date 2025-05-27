e8957640560232945cef699e0e2eae20cbcd7a68
Lynne Jones <ljones.uw@gmail.com>
2025-05-27 14:07:43 -0700

    Add new SV survey

--- sv_support.py
+++ sv_support.py
@@ -5,31 +5,36 @@ from astropy.time import Time
 import astropy
 astropy.utils.iers.conf.iers_degraded_accuracy = 'ignore'
 
-from rubin_scheduler.scheduler.utils import generate_all_sky, Footprint
-
 from rubin_scheduler.site_models import Almanac
 from rubin_scheduler.utils import Site, angular_separation
 
 from rubin_scheduler.scheduler.model_observatory import ModelObservatory, KinemModel
+from rubin_scheduler.scheduler.model_observatory import tma_movement
 from rubin_scheduler.scheduler.utils import ObservationArray
 
 from rubin_scheduler.scheduler.utils import SchemaConverter, run_info_table
 import rubin_sim.maf.plots as plots
 import rubin_sim.maf.slicers as slicers
 
+from custom_step import PerFilterStep
+from rubin_scheduler.scheduler.utils import (
+    generate_all_sky,
+    ConstantFootprint,
+    Footprint,
+    make_rolling_footprints,
+)
+
 
 __all__ = ['survey_times', 'lst_over_survey_time',
            'survey_footprint', 'setup_observatory',
            'count_obstime', 'save_opsim']
 
-def survey_times(survey_start: Time | None = None,
-                 survey_end: Time | None = None,
-                 verbose: bool = True) -> dict:
-    if survey_start is None:
-        survey_start = Time('2025-06-11T12:00:00', format='isot', scale='utc')
-    if survey_end is None:
-        survey_end = Time('2025-08-30T12:00:00', format='isot', scale='utc')
+def survey_times(verbose: bool = True, random_seed: int = 55) -> dict:
+    survey_start = Time('2025-06-15T12:00:00', format='isot', scale='utc')
+    survey_end = Time('2025-09-15T12:00:00', format='isot', scale='utc')
     survey_length = int(np.ceil((survey_end - survey_start).jd))
+
+    survey_mid = Time("2025-07-01T12:00:00", format='isot', scale='utc')
 
     if verbose:
         print("Survey start", survey_start.iso)
@@ -38,59 +43,100 @@ def survey_times(survey_start: Time | None = None,
 
     site = Site("LSST")
     almanac = Almanac(mjd_start=survey_start.mjd)
-    match = np.where(
-        (almanac.sunsets["night"] > 0) & (almanac.sunsets["night"] <= survey_length)
-    )
-    sunsets = almanac.sunsets[match]["sun_n12_setting"]
-    civil_sunsets = almanac.sunsets[match]['sunset']
-    sunrises = almanac.sunsets[match]["sun_n12_rising"]
+    alm_start = np.where(abs(almanac.sunsets['sunset'] - survey_start.mjd) < 0.5)[0][0]
+    alm_end = np.where(abs(almanac.sunsets['sunset'] - survey_end.mjd) < 0.5)[0][0]
+    alm_mid = np.where(abs(almanac.sunsets['sunset'] - survey_mid.mjd) < 0.5)[0][0]
+    mid_offset = alm_mid - alm_start
 
-    moon_illum = almanac.interpolators['moon_phase'](civil_sunsets)
+    sunsets = almanac.sunsets[alm_start:alm_end]["sun_n12_setting"]
+    civil_sunsets = almanac.sunsets[alm_start:alm_end]['sunset']
+    civil_sunrises = almanac.sunsets[alm_start:alm_end]['sunrise']
+    sunrises = almanac.sunsets[alm_start:alm_end]["sun_n12_rising"]
+    moon_set = almanac.sunsets[alm_start:alm_end]["moonset"]
+    moon_rise = almanac.sunsets[alm_start:alm_end]["moonrise"]
+    moon_illum = almanac.interpolators['moon_phase'](sunsets)
 
     survey_info = {'survey_start': survey_start,
                    'survey_end': survey_end,
                    'survey_length': survey_length,
+                   'almanac': almanac,
                    'sunsets': sunsets,
                    'sunrises': sunrises,
-                   'civil_sunsets': civil_sunsets,
+                   'moonsets': moon_set,
+                   'moonrises': moon_rise,
                    'moon_illum': moon_illum,
                    'almanac': almanac,
-                   'site': site}
+                   'site': site,
+                   'nside': 32}
 
-    # Add downtime and up/down calculation into survey_info
-    # Let's assume for the first 30 days -
-    # only the last 3 hours of the night are available
-    step1 = 30
-    down_starts = sunsets[0:step1]
-    down_ends = sunrises[0:step1] - 3.0 / 24
-    # And for the next 20 days, the first 3 hours of the night are unavailable
-    step2 = 50
-    down_starts = np.concatenate([down_starts, sunsets[step1:step2]])
-    down_ends = np.concatenate([down_ends, sunsets[step1:step2] + 3.0 / 24])
-    # And then from July through end of August, gets all of the night
-    down_starts = np.concatenate([down_starts, sunsets[step2:]])
-    down_ends = np.concatenate([down_ends, sunsets[step2:]])
 
-    # Then let's add random periods of downtime within each night
-    rng = np.random.default_rng(seed=55)
+    # Add time limits and downtime
+    # SV survey never operates during last hour of the night
+    # 2 hours per night for SV survey from June 15 - July 1
+    # choose best time (moon down)
+    # whole night from July 1 - September 15 with random downtime
+    # 25% downtime ?
+
+    rng = np.random.default_rng(seed=random_seed)
+
+    night_start = sunsets
+    night_end = sunrises - 1.0 / 24
+    # Almanac always counts moon rise and set in the current night, after sunset
+    # dark ends with either sunrise or moonrise
+    dark_end = np.where(moon_rise < night_end, moon_rise, night_end)
+    # dark starts with either sunset or moonset
+    dark_start = np.where(moon_set < night_end, moon_set, night_start)
+    # but sometimes the moon can be up the whole night
+    up_all_night = np.where(dark_end < dark_start)
+    dark_start[up_all_night] = 0
+    dark_end[up_all_night] = 0
+
+    two_hours = 'random'
+    if two_hours == 'random':
+        # Choose a random 2 hours within these times
+        obs_start = np.zeros(len(dark_start))
+        good = np.where((dark_end - dark_start - 2.5 / 24 > 0) & (night_start < survey_mid.mjd))
+        obs_start[good] = rng.uniform(low=dark_start[good], high=dark_end[good] - 2.5 / 24)
+        # Add downtime from the start of the night until the observations
+        down_starts = civil_sunsets[good]
+        down_ends = obs_start[good]
+        # Add downtime from after the observation until sunrise
+        down_starts = np.concatenate([down_starts, obs_start[good] + 2.1 / 24.0])
+        down_ends = np.concatenate([down_ends, civil_sunrises[good]])
+        # if moon up all night AND before survey_mid, remove the whole night
+        bad = np.where((dark_end - dark_start - 2.5/24 <=0) & (night_start < survey_mid.mjd))
+        down_starts = np.concatenate([down_starts, civil_sunsets[bad]])
+        down_ends = np.concatenate([down_ends, civil_sunrises[bad]])
+
+    # Then let's add random periods of downtime within each night,
     # Assume some chance of having some amount of downtime
-    # (but this is simplistic and assumes downtime once per night
-    threshold = 1.0
-    prob_down = rng.random(len(sunsets))
-    time_down = rng.gumbel(loc=0.5, scale=1, size=len(sunsets))  # in hours
-    # apply probability of having downtime or not
-    time_down = np.where(prob_down <= threshold, time_down, 0)
-    hours_in_night = (sunrises - sunsets) * 24
-    avail_in_night = hours_in_night - (down_ends - down_starts) * 24
-    time_down = np.where(time_down >= avail_in_night, avail_in_night - 0.1, time_down)
-    time_down = np.where(time_down <= 0, 0, time_down)
-    latest_start = avail_in_night - time_down
-    d_starts = rng.uniform(low=down_ends, high=down_ends + latest_start / 24.0)
-    d_ends = d_starts + time_down / 24.0
+    # (but this is simplistic and assumes downtime ~twice per night)
+    random_downtime = 0
+    for count in range(3):
+        threshold = 1.0 - (count / 5)
+        prob_down = rng.random(len(night_start))
+        time_down = rng.gumbel(loc=0.5, scale=1, size=len(night_start))  # in hours
+        # apply probability of having downtime or not
+        time_down = np.where(prob_down <= threshold, time_down, 0)
+        avail_in_night = (night_end - night_start) * 24
+        time_down = np.where(time_down >= avail_in_night, avail_in_night, time_down)
+        time_down = np.where(time_down <= 0, 0, time_down)
+        d_starts = rng.uniform(low=night_start, high=night_end - time_down / 24)
+        d_ends = d_starts + time_down / 24.0
+        # But only use these after July 15 -
+        d_starts = d_starts[mid_offset:]
+        d_ends = d_ends[mid_offset:]
+        random_downtime += ((d_ends - d_starts) * 24).sum()
+        night_hours = avail_in_night[mid_offset:].sum()
+        print(random_downtime, night_hours, random_downtime / night_hours)
+        # combine previous expected downtime and random downtime
+        down_starts = np.concatenate([down_starts, d_starts])
+        down_ends = np.concatenate([down_ends, d_ends])
 
-    # combine previous expected downtime and random downtime
-    down_starts = np.concatenate([down_starts, d_starts])
-    down_ends = np.concatenate([down_ends, d_ends])
+    # And mask the final hour of the night through the survey
+    # (already done for 0:mid)
+    down_starts = np.concatenate([down_starts, sunrises[mid_offset:] - 1.0/24])
+    down_ends = np.concatenate([down_ends, civil_sunrises[mid_offset:]])
 
     # Turn into an array of downtimes for sim_runner
     # down_starts/ down_ends should be mjd times for internal-ModelObservatory use
@@ -120,12 +166,19 @@ def survey_times(survey_start: Time | None = None,
     downtime_per_night = np.zeros(len(sunrises))
     for start, end in zip(downtimes['start'], downtimes['end']):
         idx = np.where((start > dayobsmjd) & (end < dayobsmjd + 1))
-        downtime_per_night[idx] += (end - start) * 24
+        if start < sunsets[idx]:
+            dstart = sunsets[idx]
+        else:
+            dstart = start
+        if end > sunrises[idx]:
+            dend = sunrises[idx]
+        else:
+            dend = end
+        downtime_per_night[idx] += (dend - dstart) * 24
 
-    survey_info['step1'] = step1
-    survey_info['step2'] = step2
     survey_info['downtimes'] = downtimes
     survey_info['dayobsmjd'] = dayobsmjd
+    hours_in_night = (sunrises - sunsets) * 24
     survey_info['hours_in_night'] = hours_in_night
     survey_info['downtime_per_night'] = downtime_per_night
     survey_info['avail_per_night'] = hours_in_night - downtime_per_night
@@ -160,122 +213,62 @@ def lst_over_survey_time(survey_info: dict) -> None:
     print('lst sunset @ mid', sunset_mid_lst.deg, 'lst sunrise @ mid', sunrise_mid_lst.deg)
 
 
-def survey_footprint(survey_info: dict, nside: int = 32, verbose=False):
+def survey_footprint(survey_info: dict, verbose=False):
     # Define survey footprint
 
     site = survey_info['site']
-    survey_info['nside'] = nside
-
+    nside = survey_info['nside']
     sky = generate_all_sky(nside=nside)
-
-    # Keith's suggested Wide areas - two regions ..
-    south = np.where((((sky['ra'] > 280) & (sky['ra'] <= 360)) | ((sky['ra'] >= 0) & (sky['ra'] < 10)))
-                     & (sky['dec'] < -40) & (sky['dec'] > -60))
-    north = np.where((((sky['ra'] > 310) & (sky['ra'] <= 360)) | ((sky['ra'] >= 0) & (sky['ra'] < 10)))
-                     & (sky['dec'] < 5) & (sky['dec'] > -5))
-
-    sky['map'] = np.zeros(hp.nside2npix(nside)) + np.nan
-    sky['map'][south] = 1
-    sky['map'][north] = 1
-
-    if verbose:
-        # Check individually, relevant for blob coverage..
-        area_south = len(np.where(sky['map'][south] == 1)[0]) * hp.nside2pixarea(nside, degrees=True)
-        approx_nfields_south = area_south / 9.6 * 1.3
-        print('south area', area_south, 'south approx n fields', approx_nfields_south)
-        area_north = len(np.where(sky['map'][north] == 1)[0]) * hp.nside2pixarea(nside, degrees=True)
-        approx_nfields_north = area_north / 9.6 * 1.3
-        print('north area', area_north, 'south approx n fields', approx_nfields_north)
-        # And together
-        print('total area', area_south + area_north, 'total approx n fields', approx_nfields_south + approx_nfields_north)
-
-        # How many fields are typically in a 'blob' with pair time of 33 minutes?
-        print('approx blob fields', 33  * 60 / (32 + 8))
-        # so .. looks like we'd cover the north and then south in a ~~two blobs of pairs each?
-
-        print('closest approach to ecliptic in south area (deg)?', sky['eclip_lat'][south].min())
-        print('max/min elevation in south?', abs(sky['dec'][south].max() - site.latitude),
-            abs(sky['dec'][south].min() - survey_info['site'].latitude))
-        print('max/min elevation in north?', abs(site.latitude - sky['dec'][north].min()),
-            abs(site.latitude - sky['dec'][north].max()))
-
-    # Deep field with good visibility
-    dd_ra = 310
-    dd_dec = -50
-    radius = np.sqrt(100 / np.pi)
-    dist = angular_separation(dd_ra, dd_dec, sky['ra'], sky['dec'])
-    close = np.where(dist <= radius)[0]
-    dd = np.zeros(hp.nside2npix(nside)) + np.nan
-    dd[close] = 1
+    # use ecliptic
+    sky['map'] = np.where(sky['map'] == 0, 1, np.nan)
+    sky['map'] = np.where(np.abs(sky['eclip_lat']) <= 10, sky['map'], np.nan)
+    sky['map'] = np.where((sky['eclip_lon'] > 250) | (sky['eclip_lon'] < 80), sky['map'], np.nan)
 
     survey_info['sky'] = sky
-    survey_info['deep'] = {'ra': dd_ra, 'dec': dd_dec, 'radius': radius}
-
-    if verbose:
-        area_dd = len(np.where(dd == 1)[0]) * hp.nside2pixarea(nside, degrees=True)
-        approx_nfields_dd = area_dd / 9.6 * 1.5
-        print("DDF radius", radius, "DD area, not including dither edges",
-            area_dd, "very approximate n fields", approx_nfields_dd)
-
-        # Make a nice looking figure
-        temp = np.where(np.isnan(sky['map']), 0, sky['map']) + np.where(np.isnan(dd), 0, 2 * dd)
-        temp[np.where(temp == 0)] = np.nan
-        temp = np.ma.MaskedArray(temp)
-        s = slicers.HealpixSlicer(nside=nside)
-        p = plots.HpxmapPlotter()
-        fig = p(temp, s, {'color_max': 2, 'color_min': 0, 'n_ticks': 5})
-        survey_info['figure'] = fig
 
     # Turn this into footprint_hp to use with the scheduler -
 
-    # Wide SV - goal is 60/60/60/60 visits per pointing in griz
-    band_ratios = {"u": 0, "g": 1.0, "r": 1.0, "i": 1.0, "z": 1.0, "y": 0}
+    # low-dust wfd ratios
+    low_dust_ratios={"u": 0.32, "g": 0.4, "r": 1.0, "i": 1.0, "z": 0.9, "y": 0.9}
 
     footprints_hp = np.zeros(hp.nside2npix(nside),
         dtype=list(zip(["u", "g", "r", "i", "z", "y"], [float] * 7)), )
     for b in 'ugrizy':
-        footprints_hp[b] = sky['map'] * band_ratios[b]
+        footprints_hp[b] = sky['map'] * low_dust_ratios[b]
         footprints_hp[b][np.isnan(footprints_hp[b])] = 0
 
-    if verbose:
-        print('')
-        wide_goals = [round(band_ratios[k] * 60) for k in band_ratios]
-        print('wide goal visits per filter per point', wide_goals)
+    survey_info['footprint_hp'] = footprints_hp
 
-    # Deep SV - goal is ugrizy like year two of WFD
-    # so use low-dust-ratios
-    lowdust_band_ratios = {"u": 0.35, "g": 0.4, "r": 1.0, "i": 1.0, "z": 0.9, "y": 0.9}
+    # For PerFilterStep, need to know bright/dark
+    moon_illums = survey_info['moon_illum']
+    illum_limit = 40
+    u_loaded = np.where(moon_illums <= illum_limit)[0]
+    y_loaded = np.where(moon_illums > illum_limit)[0]
 
-    dd_footprints_hp = np.zeros(hp.nside2npix(nside),
-        dtype=list(zip(["u", "g", "r", "i", "z", "y"], [float] * 7)), )
-    for b in 'ugrizy':
-        dd_footprints_hp[b] = dd * lowdust_band_ratios[b] * 3
-        dd_footprints_hp[b][np.isnan(dd_footprints_hp[b])] = 0
+    mystep = PerFilterStep(survey_length=survey_info['survey_length'],
+        loaded_dict={'u': u_loaded, 'y': y_loaded})
+    footprints = Footprint(survey_info['survey_start'].mjd, step_func=mystep)
+    for f in footprints_hp.dtype.names:
+        footprints.set_footprint(f, footprints_hp[f])
 
-    if verbose:
-        # estimates of number of visits per night / sequence for DDF (for field survey)
-        dd_goals = [round(lowdust_band_ratios[k] * 184) for k in lowdust_band_ratios]
-        print('deep goal visits per filter per point', dd_goals)
-        print("deep visits per day, 30 day survey", np.array(dd_goals) / 30, np.array(dd_goals).sum() / 30)
+    # # Use the Almanac to find the position of the sun at the start of survey
+    # sun_moon_info = survey_info['almanac'].get_sun_moon_positions(mjd_start)
+    # sun_ra_start = sun_moon_info["sun_RA"].copy()
 
-        # This would likely be the necessary visits per night that are required? Single sequence quite long.
-        seq = {'u': 16, 'g': 4, 'r': 12, 'i': 12, 'z': 12, 'y': 31}
-        # Try shorter seq, run twice.
-        dd_seq = {'u': 9, 'g': 2, 'r': 7, 'i': 8, 'z': 7, 'y': 17}
-        tt = [round(seq[k] * 1.0) for k in seq]
-        print("deep via sequence? approx ave nvis in sequence", (np.array(tt).sum() - 13) * 1.5,
-            "approx time (min) of sequence", (np.array(tt) * (32 + 5)).sum() / 60)
+    # footprints = make_rolling_footprints(
+    #     fp_hp=footprints_hp,
+    #     mjd_start=mjd_start,
+    #     sun_ra_start=sun_ra_start,
+    #     nslice=nslice,
+    #     scale=rolling_scale,
+    #     nside=nside,
+    #     wfd_indx=wfd_indx,
+    #     order_roll=1,
+    #     n_cycles=3,
+    #     uniform=rolling_uniform,
+    # )
 
-    # combined Wide and Deep footprint
-    combined_hp = np.zeros(hp.nside2npix(nside),
-        dtype=list(zip(["u", "g", "r", "i", "z", "y"], [float] * 7)), )
-    for b in 'ugrizy':
-        wide_dd_masked = np.where(dd_footprints_hp[b] > 0, 0, footprints_hp[b])
-        combined_hp[b] = wide_dd_masked + dd_footprints_hp[b]
-
-    survey_info['wide_hp'] = footprints_hp
-    survey_info['deep_hp'] = dd_footprints_hp
-    survey_info['combined_hp'] = combined_hp
+    survey_info['footprints_obj'] = footprints
 
     return survey_info
 
@@ -288,16 +281,9 @@ def setup_observatory(survey_info: dict)-> ModelObservatory:
                                    wind_data=None,
                                    downtimes = survey_info['downtimes'])
     # Slow the telescope down with smaller jerk/acceleration and maxvel
+    tma = tma_movement(70)
     # But is faster with settle of 0 (Tiago is working on this)
-    model_obs.setup_telescope(
-        altitude_maxspeed=2.0,
-        altitude_accel=2.0,
-        altitude_jerk=8.0,
-        azimuth_maxspeed=2.0,
-        azimuth_accel=2.0,
-        azimuth_jerk=8.0,
-        settle_time=0.0,
-    )
+    model_obs.setup_telescope(**tma)
     return model_obs
 
 
